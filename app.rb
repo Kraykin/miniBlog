9335cad0e454834f857124a6b24e6a99b05db65d
Kraykin <kraykin@gmail.com>
2016-01-27 18:52:59 +0300

    Add simple /post/ ... post handler

--- app.rb
+++ app.rb
@@ -59,3 +59,10 @@ get '/post/:post_id' do
 
 	erb :post
 end
+
+post '/post/:post_id' do
+	post_id = params[:post_id]
+	content = params[:content]
+
+	erb "You typed: #{content} for #{post_id}"
+end
